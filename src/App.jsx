820cebe3a87a2b1b7a038cda89fd168245d7f8fa
Dani Atanasova <dani@uniquevisitors.com>
2022-06-13 18:36:20 +0300

    Fix Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Login } from "./components/auth/login/Login";
 import { Register } from "./components/auth/register/Register";
 import { Layout } from "./components/layout/Layout";
+import { AuthenticatedRoute } from "./utils/guards/authenticatedRoute";
 
 function App() {
   return (
@@ -10,7 +11,11 @@ function App() {
       <Routes>
         <Route exact path="/register" element={<Register/>} />
         <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/*" element={<Layout/>} />
+        <Route exact path="/*" element={
+          <AuthenticatedRoute>
+            <Layout/>
+          </AuthenticatedRoute>
+        } />
       </Routes>
     </div>
   );
