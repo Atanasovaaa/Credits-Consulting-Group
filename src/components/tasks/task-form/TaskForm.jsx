5e6343fda774151b3910478ab457ccdefc77363e
Dani Atanasova <dani@uniquevisitors.com>
2022-06-14 17:11:22 +0300

    Add Create for Task

---
+++ src/components/tasks/task-form/TaskForm.jsx
@@ -0,0 +1,65 @@
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./taskForm.scss";
+import { useState } from "react";
+import { saveTask, TaskStatus } from "../../../utils/http-utils/task-request";
+import { useNavigate } from "react-router-dom";
+
+export function TaskForm() {
+
+    const navigate = useNavigate();
+
+    const [task, setTask] = useState({
+        title: '',
+        description: '',
+        status: '',
+        dueDate: ''
+    });
+
+    const onInputChange = (event) => {
+        setTask((prevState) => (
+            {
+                ...prevState,
+                [event.target.name]: event.target.value
+            }
+        ));
+    }
+
+    const onTaskSubmit = (event) => {
+        event.preventDefault();
+
+        saveTask(task).then(() => {
+            console.log("Success");
+            navigate("/tasks-list");
+        });
+    }
+
+
+    
+    return (
+        <div className="task-form-wrapper">
+            <h2>Create Task</h2>
+            <Form onSubmit={onTaskSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicTitle">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter Title" name="title" value={task.title || ''} onChange={onInputChange}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control type="text" placeholder="Enter description" name="description" value={task.description || ''} onChange={onInputChange}/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Status</Form.Label>
+                    <Form.Select placeholder="Select Status" name="status" value={task.status} aria-label="Select Status" onChange={onInputChange}>
+                        { Object.keys(TaskStatus).map(status => <option key={status} value={TaskStatus[status]}>{TaskStatus[status]}</option>) }
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicDueDate">
+                    <Form.Label>Due Date</Form.Label>
+                    <Form.Control type="date" placeholder="Due Date" name="dueDate" value={task.dueDate || ''} onChange={onInputChange}/>
+                </Form.Group>
+                <Button variant="success" type="submit">Save</Button>
+            </Form>
+        </div>
+    );
+}
